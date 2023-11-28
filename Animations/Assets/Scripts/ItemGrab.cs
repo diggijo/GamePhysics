6668e4b726f225713cb2913c920d015b6aa24217
STUDENT Joseph Diggins <Joseph.Diggins@students.ittralee.ie>
2023-11-28 17:54:06 +0000

    Added new scene for IK movement.
    Signed-off-by: STUDENT Joseph Diggins <Joseph.Diggins@students.ittralee.ie>

---
+++ Animations/Assets/Scripts/ItemGrab.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrab : MonoBehaviour
+{
+    private bool canCollect = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Can pick up");
+            canCollect = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Cannot pick up");
+            canCollect = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canCollect)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
